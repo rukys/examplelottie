1e3601f9a6d7721b2c64e79c1c88f657a55ec655
rukys <sektyawan33@gmail.com>
2025-07-29 16:48:01 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  theme: {
+    screens: {},
+    colors: {
+      green: '#75a343',
+      white: '#FFFFFF',
+      black: '#000000',
+    },
+    fontFamily: {
+      montserrat: ['Montserrat-Regular'],
+      montserratMedium: ['Montserrat-Medium'],
+      montserratSemiBold: ['Montserrat-SemiBold'],
+      montserratBold: ['Montserrat-Bold'],
+      montserratExtraBold: ['Montserrat-ExtraBold'],
+      montserratLight: ['Montserrat-Light'],
+      montserratExtraLight: ['Montserrat-ExtraLight'],
+      montserratThin: ['Montserrat-Thin'],
+    },
+    extend: {},
+  },
+};
